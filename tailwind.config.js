040e819e123c98f359ec88d35e16fa3e1825af93
Emmanuel Akinyemi <58298846+emmanuel-akinyemi-Dev@users.noreply.github.com>
2023-12-28 21:13:56 +0100

    service page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","./public/message.html",'./public/about.html', './public/handles.html', "./public/portfolio.html", "./public/portfolio2.html"],
+  content: ["index.html",
+  "./public/message.html",
+  './public/about.html', 
+  './public/handles.html',
+  "./public/portfolio.html",
+  "./public/portfolio2.html",
+  "./public/service.html"],
   theme: {
     extend: {
       width:{
